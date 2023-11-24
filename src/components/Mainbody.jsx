63cca3912b2e55c9b64c92900a95d309d2708d5f
22Ahmii <ahmarqasim99@gmail.com>
2023-11-24 12:19:59 +0500

    main code structure

---
+++ src/components/Mainbody.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from "../components/Navbar";
+import main from '../assests/logo/main.png'; // Replace this with your image path
+
+export default function Mainbody() {
+    return (
+
+        <div style={{ position: 'relative' }}>
+            <img src={main} alt="Main" />
+            <div style={{ position: 'absolute', top: 0 }} className='md:lg:left-[-20px]  md:2xl:left-[100px]' >
+                <Navbar /></div>
+            <div className=' absolute text-center top-20 md:lg:top-60 md:lg:left-[300px] md:2xl:left-[420px]'>
+                <h1 className='md:text-8xl text-5xl text-white'>HOAs Made Simple</h1>
+                <button className="bg-blue-400 mt-4 font-semibold rounded-full px-6 py-2">
+                    Learn More
+                </button>
+
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Mainbody from '../components/Mainbody'
+function Home() {
+  return (
+    <div>
+         <Mainbody/>
+    </div>
+  )
+}
+
+export default Home
