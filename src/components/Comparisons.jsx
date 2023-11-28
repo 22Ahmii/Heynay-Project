6234d19eda84de1064222bfbb9902cb15743e1f5
22Ahmii <ahmarqasim99@gmail.com>
2023-11-28 17:44:43 +0500

    comparisons desgn

--- src/components/Comparisons.jsx
+++ src/components/Comparisons.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import vector1 from '../assests/logo/vector1.png'
 import eyes1 from '../assests/images/eyes1.png'
+import image6 from '../assests/images/image6.png'
 import { FaCheck } from "react-icons/fa";
 export default function Comparisons() {
     return (
@@ -12,14 +13,16 @@ export default function Comparisons() {
             </div>
         <div className='lg:w-[1000px] p-[40px] lg:p-0 lg:ml-[200px]  2xl:ml-[380px] '>
             <div className="relative mt-40  p-10 overflow-x-auto shadow-md sm:rounded-lg bg-blue-50 2xl:p-16">
-                <table className="lg:w-[900px] 2xl:ml-[-40px]  text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed">
-                    <thead class="text-xs text-blue-950 uppercase  dark:text-gray-400">
+                <table className="lg:w-[900px] 2xl:ml-[-40px]  text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+                    <thead class="text-xs text-blue-950 uppercase   dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
                                
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                Heynay
+                            
+                            <th scope="col" class="px-6 rounded-lg  py-3 bg-white">
+                            <img src={image6} className='h-12 lg:ml-4'/>
+                                <span className='lg:ml-3'>Heynay</span>
                             </th>
                             <th scope="col" class="px-6 py-3">
                             Social Media
@@ -37,8 +40,8 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                 Cost
                             </th>
-                            <td class="px-8 py-4 text-blue-900">
-                               Free
+                            <td class="px-8 py-4 text-blue-900 bg-white">
+                               <span className='lg:ml-3'> Free</span>
                             </td>
                             <td class="px-12 py-4 text-blue-900">
                                Free
@@ -54,9 +57,9 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                 Direct Messaging
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 py-2 bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
                             <td class="px-12  text-blue-900 py-4">
                             <FaCheck />
                             </td>
@@ -71,9 +74,9 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                Event Scheduling
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 py-2 bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
                             <td class="px-12  text-blue-900 py-4">
                             <FaCheck />
                             </td>
@@ -88,9 +91,10 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Polls & Surveys
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 py-2 bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
+
                             <td class="px-12  text-blue-900 py-4">
                             <FaCheck />
                             </td>
@@ -105,9 +109,9 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                 Organized Message Board/Feed
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 py-2 bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
                             <td class="px-6 py-4">
                               
                             </td>
@@ -122,9 +126,9 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                 Customization and Branding
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 py-2 bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
                             <td class="px-6 py-4">
                              
                             </td>
@@ -139,9 +143,9 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                 Privacy & Security
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 py-2 bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
                             <td class="px-6 py-4">
                                
                             </td>
@@ -156,9 +160,9 @@ export default function Comparisons() {
                             <th scope="row" class="px-6 py-4 font-medium text-blue-950 whitespace-nowrap dark:text-white">
                                 Dedicated HOA Support
                             </th>
-                            <td class="px-8 text-blue-900 py-4">
-                            <FaCheck />
-                            </td>
+                            <td className="px-6 text-blue-900 rounded-lg bg-white">
+                            <FaCheck className='lg:ml-6 ml-3 ' style={{ fontSize: '1.5em'}} />
+                              </td>
                             <td class="px-6 py-4">
                              
                             </td>
